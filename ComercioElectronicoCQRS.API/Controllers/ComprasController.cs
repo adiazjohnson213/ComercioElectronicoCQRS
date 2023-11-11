171c383090fa11d0a4dee125af6c98549a9a9b80
Arthur Diaz <adiazjohnson213@gmail.com>
2023-11-11 13:08:02 -0500

    Add project files.

---
+++ ComercioElectronicoCQRS.API/Controllers/ComprasController.cs
@@ -0,0 +1,21 @@
+﻿using Dominio.Querys.Productos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ComercioElectronicoCQRS.Controllers
+{
+    [ApiController]
+    [Route("api/{controller}")]
+    public class ComprasController : ControllerBase
+    {
+        public ComprasController()
+        {
+
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ObtenerProductos(ObtenerProductosQuery obtenerProductosQuery)
+        {
+            
+        }
+    }
+}
---
+++ ComercioElectronicoCQRS.Infraestructura/ComercioElectronicoContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Infraestructura
+{
+    public class ComercioElectronicoContext : DbContext
+    {
+        public ComercioElectronicoContext(DbContextOptions<ComercioElectronicoContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
